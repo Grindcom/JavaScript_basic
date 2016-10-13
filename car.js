a71f5624c85153eead0f8d7f03a29dbc7482c7ac
Greg Ford <ford@grindcom.ca>
2016-10-13 10:47:08 -0700

    Test write and log functions.

--- car.js
+++ car.js
@@ -1,3 +1,11 @@
 // From Udacity, Front-end Developer:
 // Object Oriented JavaScript course
 // Additional code by: Greg Ford
+
+// document.write("hello world.");
+
+var fn = function(one, two){
+    console.log(this, one, two);
+    document.write(this, one, two);
+}
+fn("red","blue");
